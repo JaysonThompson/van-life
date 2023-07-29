b9b39e4f50a443e1ffbb0b2faf1f15a383b8d872
Jayson Thompson <jaysonthompson922@gmail.com>
2023-07-29 06:20:44 -0700

    refactoring directories for layout routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import Home from "./components/Home";
-import About from "./components/About";
-import Vans from "./components/Vans";
-import VanDetails from "./components/VanDetails";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import VanDetails from "./pages/VanDetails";
 
 import "../server";
 
