988dcf8b141273d622873451317fd11026d4a39a
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-01 13:07:44 -0700

    adding NavLinks for active styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Nav() {
+  const active = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <header>
       <Link className="site-logo" to="/">
         #VanLife
       </Link>
       <nav>
-        <Link to="host">Hosts</Link>
-        <Link to="about">About</Link>
-        <Link to="vans">Vans</Link>
+        <NavLink to="host" style={({ isActive }) => (isActive ? active : null)}>
+          Hosts
+        </NavLink>
+        <NavLink
+          to="about"
+          style={({ isActive }) => (isActive ? active : null)}
+        >
+          About
+        </NavLink>
+        <NavLink to="vans" style={({ isActive }) => (isActive ? active : null)}>
+          Vans
+        </NavLink>
       </nav>
     </header>
   );
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,33 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
+  const active = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <div>
       <nav className="host-nav">
-        <Link to="/host">Dashboard</Link>
-        <Link to="income">Income</Link>
-        <Link to="reviews">Reviews</Link>
+        <NavLink
+          to="/host"
+          end
+          style={({ isActive }) => (isActive ? active : null)}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="income"
+          style={({ isActive }) => (isActive ? active : null)}
+        >
+          Income
+        </NavLink>
+        <NavLink
+          to="reviews"
+          style={({ isActive }) => (isActive ? active : null)}
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </div>
